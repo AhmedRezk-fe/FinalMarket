bb716ba7422734709730e6cf16569049836bf4ec
Ahmed Rizk <ahmed.rezk.fe@gmail.com>
2020-10-27 21:18:17 +0200

    fils

--- js/script.js
+++ js/script.js
@@ -1,4 +1,61 @@
 $(window).on("load", function () {
+
+    $(".Summary-product-drop-header").click(function(){
+        $(this).parent().children(".Summary-product-drop-content").slideToggle(150)
+    })
+    $("li.header-box").click(function(){
+        // $(".list-box").not($(this).parent().toggleClass("active")).removeClass("active");
+        // $("ul.list-box li.body-box").not($(this).parent().children("li.body-box").slideToggle(300)).slideUp(100)
+
+        $(this).parent().children("li.body-box").slideToggle(300)
+        $(this).parent().toggleClass("active")
+    })
+
+    $(".btn-add-department button").click(function(e){
+        $(".sidebar-modal").addClass("active");
+        $('body').addClass("overlay");
+        e.preventDefault()
+    })
+    $(".overlay-sidebar").click(function(){
+        $(this).parent().removeClass("active");
+        $('body').removeClass("overlay");
+    })
+
+
+
+
+    // var n = 0;
+    
+    $( "li.dropdown-menu-small .dropdown" )
+    .mouseenter(function() {
+        // alert("sad")
+        // n += 1;
+        $( this ).children( ".menu-small" ).addClass( "active" );
+    })
+    .mouseleave(function() {
+        $( this ).children( ".menu-small" ).removeClass("active");
+    });
+
+
+    $( "li.dropdown" )
+    .mouseenter(function() {
+        // n += 1;
+        $( this ).children( ".dropdown-menu" ).addClass( "active" );
+    })
+    .mouseleave(function() {
+        $( this ).children( ".dropdown-menu" ).removeClass("active");
+    });
+
+
+
+
+    if($(".sign_in").height() > $(window).height()){
+        $(".sign_in").removeClass("authwindo")
+
+    }else{
+        $(".sign_in").addClass("authwindo")
+    }
+
     // loading
     // $('.pre-load').stop().animate({
     //     opacity: 0
@@ -41,22 +98,49 @@ $(window).on("load", function () {
         }
     });
 
+    $('.slider-auth-owl').owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        rtl: true,
+        autoplay: false,
+        // animate: true,
+        navText: [
+            "<i class='fa fa-angle-right' aria-hidden='true'></i>",
+            "<i class='fa fa-angle-left' aria-hidden='true'></i>"
+        ],
+        responsive: {
+            400: {
+                items: 1
+            },500: {
+                items: 1
+            },700: {
+                items: 1
+            },800: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
 
     // nav 
-    $(".click--nav").click(function () {
+    $(".click-nav").click(function () {
         $(this).toggleClass("open");
         $(".click--nav").toggleClass("active");
-        $(".body-overlay").toggleClass("back");
-        $(".nav").toggleClass("back");
+        $(".overlay-header").toggleClass("block-nav");
+        $(".nav").toggleClass("block-nav");
         $('body').toggleClass("overlay");
     });
 
 
-    $(".body-overlay").click(function () {
-        $(".click--nav").removeClass("open");
-        $(".click--nav").removeClass("active");
-        $(".body-overlay").removeClass("back");
-        $(".nav").removeClass("back");
+    $(".overlay-header").click(function () {
+        $(".click-nav").removeClass("open");
+        $(".click-nav").removeClass("active");
+        $(".overlay-header").removeClass("block-nav");
+        $(".nav").removeClass("block-nav");
         $('body').removeClass("overlay");
     });
 
